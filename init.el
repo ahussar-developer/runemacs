0a38a55d8518042d981d17e8e11b80b19e2024a9
alex-arco-laptop <alexandrahussar01@gmail.com>
2021-07-06 12:10:54 -0700

    3 videos worth of settings from emacs from scratch youtube

--- init.el
+++ init.el
@@ -7,7 +7,6 @@
 
 (menu-bar-mode -1) ; Disable menu bar
 
-(load-theme 'deeper-blue)
 
 ;; Make ESC quit prompts
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
@@ -15,8 +14,8 @@
 (set-face-attribute 'default nil :font "FiraCode Nerd Font Mono" :height 125)
 ;(set-face-attribute 'default nil :font "DejaVu Sans" :height 125)
 
-(set-face-attribute 'mode-line nil
-                    :background "#666666")
+;(set-face-attribute 'mode-line nil
+;                   :background "#666666")
 
 ;; initialize package source
 (require 'package)
@@ -36,10 +35,19 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-;; Show command key binding
-(use-package command-log-mode)
+(column-number-mode)
+(global-display-line-numbers-mode t)
 
-(use-package counsel)
+;; Disable line numbers for some modes
+(dolist (mode '(org-mode-hook
+		term-mode-hook
+		shell-mode-hook
+		eshell-mode-hook))
+  (add-hook mode(lambda () (display-line-numbers-mode 0))))
+
+;; Show command key binding
+;(use-package command-log-mode)
+
 (use-package all-the-icons)
 
 (use-package ivy
@@ -59,7 +67,93 @@
   :config
   (ivy-mode 1))
 
+(global-set-key (kbd "C-M-j") 'counsel-switch-buffer)
+(global-set-key (kbd "C-s-e") 'eval-last-sexp)
+
+
 (use-package doom-modeline
   :ensure t
   :init (doom-modeline-mode 1)
   :custom ((doom-modeline-height 15)))
+
+(use-package doom-themes
+  :config
+  (load-theme 'doom-palenight t))
+
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 0.3))
+
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1))
+
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+	  ("C-x b" . counsel-ibuffer)
+	  ("C-x C-f" . counsel-find-file)
+	 :map minibuffer-local-map
+	 ("C-r" . counsel-minibuffer-history)))
+
+(use-package helpful
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
+
+
+(use-package general
+  :config
+  (general-create-definer rune/keys
+    :keymaps '(normal insert visual emacs)
+    :prefix "SPC"
+    :global-prefix "C-SPC")
+  
+  (rune/keys
+   "t" '(:ignore t :which-key "toggles")
+   "tt" '(counsel-load-theme :which-key "choose theme")))
+
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump nil)
+  :config
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+
+  ;; Use visual line motions even outside of visual-line-mode buffers
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal))
+
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
+
+
+(use-package hydra)
+
+(defhydra hydra-text-scale (:timeout 4)
+  "scale text"
+  ("j" text-scale-increase "in")
+  ("k" text-scale-decrease "out")
+  ("f" nil "finished" :exit t))
+
+(rune/keys
+  "ts" '(hydra-text-scale/body :which-key "scale text"))
